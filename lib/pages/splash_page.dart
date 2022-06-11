26ab12f28144ae1804320deddc44623ff9c73441
kedrakk <keaindrak@gmail.com>
2022-06-11 14:30:29 +0630

    fix splash error

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -3,21 +3,41 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:kedk_portfolio/pages/home_page.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    Timer(
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  late Timer _timer;
+
+  @override
+  void initState() {
+    _timer = Timer(
       const Duration(seconds: 3),
       () => Navigator.pushReplacement(
         context,
         MaterialPageRoute(
           builder: (context) => HomePage(),
         ),
+        result: 4,
       ),
     );
+    super.initState();
+  }
 
+  @override
+  void dispose() {
+    if (_timer.isActive) {
+      _timer.cancel();
+    }
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Text(
